1d753774fc19034c2afe28bb9c5b001f64089196
Cesar <50472267+EmilCes@users.noreply.github.com>
2025-01-18 17:53:52 -0600

    Delete teacher column from educative experience

--- public/assets/js/registroExperienciaEducativa.js
+++ public/assets/js/registroExperienciaEducativa.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-    $("#profesor").select2();
     $("#programa").select2();
     $("#enviar").on("click", function (e) {
         e.preventDefault();
@@ -10,13 +9,12 @@ $(document).ready(function () {
 function validarFormulario() {
     var nombre = $('#nombre').val();
     var nrc = $('#nrc').val();
-    var profesor = $('#profesor').val();
     var programa = $('#programa').val();
 
     $('.form-control').removeClass("borderRed borderGreen");
 
     var error = false;
-    if (!nombre || !nrc || !profesor || !programa) {
+    if (!nombre || !nrc || !programa) {
         Swal.fire({
             title: '¡Error!',
             icon: 'error',
@@ -27,7 +25,6 @@ function validarFormulario() {
 
         if (!nombre) $('#nombre').addClass("borderRed");
         if (!nrc) $('#nrc').addClass("borderRed");
-        if (!profesor) $('#profesor').addClass("borderRed");
         if (!programa) $('#programa').addClass("borderRed");
 
         error = true;
@@ -37,7 +34,7 @@ function validarFormulario() {
         return;
     }
 
-    $('#nombre, #nrc, #profesor, #programa').removeClass("borderRed").addClass("borderGreen");
+    $('#nombre, #nrc, #programa').removeClass("borderRed").addClass("borderGreen");
 
     $("#form").submit();
 }
