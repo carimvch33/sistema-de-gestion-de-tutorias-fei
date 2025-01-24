df13dac615d85e03ab61ee80bc6115d5619264fb
Cesar <50472267+EmilCes@users.noreply.github.com>
2025-01-24 11:06:12 -0600

    Completed

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 define('ENVIRONMENT', 'development'); // development || production
-define('BASE_URL', '/agendatutorias'); //agendatutorias
+define('BASE_URL', '/agendatutorias2'); //agendatutorias
 define('DB_HOST', '10.37.129.2'); //localhost
 
 function isProduction()
