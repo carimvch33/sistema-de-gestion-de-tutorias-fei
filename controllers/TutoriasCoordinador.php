4ba9f4e70cd2dc624fd95f3ba725f7689c6ab1e1
engaherr <zS21013858@estudiantes.uv.mx>
2025-04-22 09:07:12 -0600

    feat(reporte): implements show reports methods

--- controllers/TutoriasCoordinador.php
+++ controllers/TutoriasCoordinador.php
@@ -3,18 +3,21 @@ require_once '../config/config.php';
 require_once '../config/connection.php';
 require_once '../models/Tutoria.php';
 require_once '../models/Coordinador.php';
+require_once '../models/Reporte.php';
 
 class TutoriasCoordinador
 {
     private $conn;
     private $tutoriaModel;
     private $coordinadorModel;
+    private $reportesModel;
 
     public function __construct()
     {
         $this->conn = connectiondb();
         $this->tutoriaModel = new Tutoria($this->conn);
         $this->coordinadorModel = new Coordinador($this->conn);
+        $this->reportesModel = new Reporte($this->conn);
     }
 
     public function showTutorias()
@@ -50,5 +53,64 @@ class TutoriasCoordinador
         require_once '../views/consultarTutorias.php';
     }
 
+    public function showReportesTutorias()
+    {
+        session_start();
+
+        $rolesPermitidos = [4];
+        if (!isset($_SESSION['user']) || !in_array($_SESSION["rol"], $rolesPermitidos)) {
+            header('Location: ' . BASE_URL . '/cerrarSesion.php');
+            exit();
+        }
+
+        $user = $_SESSION['user'];
+        $correoCoordinador = $_SESSION['correoInstitucional'];
+
+        $csrf_token = $_SESSION['csrf_token'] ?? bin2hex(random_bytes(32));
+        $_SESSION['csrf_token'] = $csrf_token;
+
+        $idSesion = $this->coordinadorModel->getIdSesionByCorreo($correoCoordinador);
+
+        if ($idSesion) {
+            $_SESSION['idSesion'] = $idSesion;
+
+            $reportes = $this->reportesModel->getReportesByCoordinador($idSesion);
+        } else {
+            $reportes = [];
+        }
+
+        $muestraActual = true;
+        require_once '../views/consultarReportes.php';
+    }
+
+    public function showHistorialReportesTutorias()
+    {
+        session_start();
+
+        $rolesPermitidos = [4];
+        if (!isset($_SESSION['user']) || !in_array($_SESSION["rol"], $rolesPermitidos)) {
+            header('Location: ' . BASE_URL . '/cerrarSesion.php');
+            exit();
+        }
+
+        $user = $_SESSION['user'];
+        $correoCoordinador = $_SESSION['correoInstitucional'];
+
+        $csrf_token = $_SESSION['csrf_token'] ?? bin2hex(random_bytes(32));
+        $_SESSION['csrf_token'] = $csrf_token;
+
+        $idSesion = $this->coordinadorModel->getIdSesionByCorreo($correoCoordinador);
+
+        if ($idSesion) {
+            $_SESSION['idSesion'] = $idSesion;
+
+            $reportes = $this->reportesModel->getHistorialReportesByCoordinador($idSesion);
+        } else {
+            $reportes = [];
+        }
+
+        $muestraActual = false;
+        require_once '../views/consultarReportes.php';
+    }
 }
 ?>
